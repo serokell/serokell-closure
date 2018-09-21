5eb6b8b612b52a8e870e38c7a5228f119dc40524
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-21 12:49:54 +0000

    Update Nixpkgs pin

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 fetchGit {
   url = https://github.com/serokell/nixpkgs;
-  ref = "20180921.111232";
-  rev = "97d92b675274e4acbe7be4f4c9eafc6318c3a964";
+  ref = "20180921.124907";
+  rev = "72fea603ef50b1ea04b67b470c1f0d094895c615";
 }
