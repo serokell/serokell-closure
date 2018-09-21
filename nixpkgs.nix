b561fa895446c2c5e43cc918e0250798b77b404f
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-21 14:22:02 +0000

    Bump Nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 fetchGit {
   url = https://github.com/serokell/nixpkgs;
-  ref = "20180921.124907";
-  rev = "72fea603ef50b1ea04b67b470c1f0d094895c615";
+  rev = "20180921.142049";
+  rev = "c7b881f402b5abfbcbdcd7eb041498e84ba367d0";
 }
