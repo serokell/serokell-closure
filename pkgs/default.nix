6e939775b67ac233be24fc2cdb8270c7394d33f2
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-22 19:45:39 +0000

    Add braces around inherited attrset

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -21,7 +21,7 @@ in
     rev = "192f3c22b4270be84aef9176fdf52a41d0d85b32";
   }) {};
 
-  inherit gitignore gitignoreSource gitignoreSourceAux;
+  inherit (gitignore) gitignoreSource gitignoreSourceAux;
 
   mixToNix = callPackage (fetchGit {
     url = https://github.com/serokell/mix2nix;
