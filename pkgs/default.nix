82f33fedcd8820269520a2c32dce94019a2e4ed1
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-09-30 16:27:40 +0000

    Revert "Bump stack-to-nix (-Wall)"
    This reverts commit bbb0d72d200bfdec22964b97ea24aadf16c01335.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -51,6 +51,6 @@ in
 
   stackToNix = import (fetchGit {
     url = https://github.com/serokell/stack4nix;
-    rev = "c0ebe262039846feec9e9c6414bf5f00cdfc3ba9";
+    rev = "e227092e52726cfd41cba9930c02691eb6e61864";
   }) { pkgs = final; };
 }
