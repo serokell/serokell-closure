ee7a40f6bf4de2e5fae9baee34837620c76c7dc2
Felix C. Stegerman <flx@obfusk.net>
2018-10-01 19:59:51 +0200

    constGitIgnore: add documentation, do not add .gitignore twice

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -26,15 +26,26 @@ in
   gitIgnore = root: aux:
     gitignore.gitignoreSourceAux (aux ++ [ ".git" ]) root;
 
+  # `constGitIgnore pathname root aux` returns a builtins.path
+  # (setting the name to pathname so that the source directory name is
+  # constant and does not depend on the current directory name),
+  # filtering out not just .git directories (using
+  # `lib.cleanSourceFilter`) but also files ignored by git (using
+  # siers' nix-gitignore).  For example, to use the name "fooBar" and
+  # the current directory and not add any additional gitignores
+  # besides those in the .gitignore file, use:
+  #
+  #   constGitIgnore "fooBar" ./. []
+  #
   constGitIgnore = with gitignore; pathname: root: aux:
     builtins.path {
       name    = pathname;
       path    = root;
-      filter  =
-        let ign = lib.toList aux ++ [(root + "/.gitignore")];
-        in  (name: type:
-              gitignoreFilter (gitignoreCompileIgnore ign root) root name type
-              && lib.cleanSourceFilter name type);
+      filter  = name: type:
+        gitignoreFilter (gitignoreCompileIgnore aux root) root name type
+        && lib.cleanSourceFilter name type;
+        # NB: gitignoreCompileIgnore currently adds .gitignore (as
+        # does gitignoreFilterSourceAux), which may be a bug
     };
 
   darwin = previous.darwin // {
