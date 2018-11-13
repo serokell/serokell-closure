e70fffe0e6b1b8543806c87adabf916526adb711
Yorick van Pelt <yorick@yorickvanpelt.nl>
2018-11-13 15:17:16 +0100

    Bump nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 fetchGit {
   url = https://github.com/serokell/nixpkgs;
-  ref = "20181023.165110";
-  rev = "361a42b57897de69dfbae528142e4b414e78ea01";
+  ref = "20181113151712";
+  rev = "6ba8bb0f68e0987546df717fad28b4110e6ee4cb";
 }
