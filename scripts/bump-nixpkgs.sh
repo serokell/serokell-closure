89c12944438140fb66f047388181add3542479bf
Christian Höppner <christian.hoppner@serokell.io>
2018-11-28 15:47:37 +0000

    bump-nixpkgs.nix: Rewrite
    * Conform to shellcheck
    * Confirm before commit, show diff
    * Allow overriding $NIXPKGS path
    * Respect $DEBUG
    * Use forced clobber to overwrite nixpkgs.nix

--- scripts/bump-nixpkgs.sh
+++ scripts/bump-nixpkgs.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
-set -eux
-pushd ../nixpkgs
-TAG=$(date +%FT%T | tr -cd '[0-9]')
-git tag "$TAG"
+set -euo pipefail
+[[ ! -z ${DEBUG:-} ]] && set -x
+
+# Override with `NIXPKGS=/path/to/nixpkgs bump-nixpkgs.sh`
+[[ -z ${NIXPKGS:-} ]] && NIXPKGS=$(readlink -f ../nixpkgs)
+TAG=$(date +"%Y%m%d%H%M%S")
+
+pushd "$NIXPKGS"
 COMMIT=$(git rev-parse HEAD)
+git tag "$TAG"
 git push git@github.com:serokell/nixpkgs.git "$TAG"
 popd
-cat <<EOF > ./nixpkgs.nix
+
+cat <<EOF >| ./nixpkgs.nix
 fetchGit {
   url = https://github.com/serokell/nixpkgs;
   ref = "$TAG";
   rev = "$COMMIT";
 }
 EOF
-git commit ./nixpkgs.nix -m "Bump nixpkgs"
+
+git --no-pager diff ./nixpkgs.nix
+
+echo
+read -p "Are you sure you want to commit this? [y/N]" -n 1 -r
+[[ $REPLY =~ ^[Yy]$ ]] && git commit ./nixpkgs.nix -m "Bump nixpkgs"
