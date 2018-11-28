533748f4e12c32eee9ae6e0150db32f08bf1335e
Christian Höppner <christian.hoppner@serokell.io>
2018-11-28 15:30:05 +0000

    generate-import.sh: Quoting, error message

--- scripts/generate-import.sh
+++ scripts/generate-import.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
+set -eou pipefail
+[[ ! -z ${DEBUG:-} ]] && set -x
+
 REPO=serokell/serokell-closure
-REV=$(curl -s https://api.github.com/repos/$REPO/branches/master | jq -r .commit.sha)
+REV=$(curl -s "https://api.github.com/repos/$REPO/branches/master" | jq -r .commit.sha)
+REF=$(curl -s "https://api.github.com/repos/$REPO/tags" | jq -r 'map(select(.commit.sha == "'"$REV"'"))[0].name')
+
+if [[ $REF == null ]]; then
+    echo "No tag found for $REV."
+    # shellcheck disable=SC2016
+    echo 'Create one with: git tag $(date +"%Y%m%d%H%M%S"); git push $_'
+    exit 1
+fi
+
 cat <<EOF
 let
   pkgs = import (fetchGit {
     url = https://github.com/$REPO;
     rev = "$REV";
-    ref = "$(curl -s https://api.github.com/repos/$REPO/tags | jq -r 'map(select(.commit.sha == "'$REV'"))[0].name')";
+    ref = $REF;
   });
 in
 EOF
