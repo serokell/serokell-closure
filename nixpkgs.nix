42a7a188bb5e71b22353c522a6396fb86ffab3c5
Christian Höppner <christian.hoppner@serokell.io>
2019-07-04 16:39:37 +0100

    Bump nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,5 +1,5 @@
 fetchGit {
   url = https://github.com/serokell/nixpkgs;
-  ref = "20190701184454";
-  rev = "d6ebc1a3e120de7b039886de5c5f6b4dcde747d3";
+  ref = "20190704163931";
+  rev = "39079c7b4cb7ca561bfc7cf3f613a16f9d033714";
 }
